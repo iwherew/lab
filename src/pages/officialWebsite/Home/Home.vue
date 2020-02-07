78394ed88a526df3afe743b4eaae22ca2e126f51
iwhere <iwhere_w@126.com>
2020-02-07 16:23:06 +0800

    首页-荣誉奖项

--- src/pages/officialWebsite/Home/Home.vue
+++ src/pages/officialWebsite/Home/Home.vue
@@ -4,6 +4,7 @@
     <carousel/>
     <introduce/>
     <research/>
+    <awards/>
   </div>
 </template>
 <script>
@@ -11,8 +12,9 @@
   import carousel from "./carousel";
   import introduce from "./introduce";
   import research from "./research";
+  import awards from "./awards";
   export default {
-    components: {labNav,carousel,introduce,research},
+    components: {labNav,carousel,introduce,research,awards},
   }
 </script>
 <style scoped lang="less">
